507ff9eb3cd134d1de896687a9aa1ccc3b8d3183
adrien.mechin@gmail.com <adrien.mechin@gmail.com>
2019-10-01 18:11:08 +0200

    Ex2 - Basic Training C# absolute difference

---
+++ Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(1, 2));
+            Console.ReadKey();
+        }
+
+        public static int test(int x, int y)
+        {
+            return x == y ? (x + y) * 3 : x + y;
+        }
+    }
+}
