c54c375b5525786af8e73d6c8f1ac95c2897f2ff
adrien.mechin@gmail.com <adrien.mechin@gmail.com>
2019-10-01 18:20:53 +0200

    Ex3 - BasicAlgo - Conditions

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -10,13 +10,25 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(test(1, 2));
+            //Console.WriteLine(sum(1, 2));
+            Console.WriteLine(diff51(56));
             Console.ReadKey();
         }
 
-        public static int test(int x, int y)
+        // Exercice 1
+        public static int sum(int x, int y)
         {
             return x == y ? (x + y) * 3 : x + y;
         }
+
+        /* Exercice 2
+         * Write a C# Sharp program to get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference
+         */
+        public static int diff51(int n)
+        {
+            return n > 51 ? (n - 51) * 3 : 51 - n;
+        }
+
+
     }
 }
---
+++ Ex3/Program.cs
@@ -0,0 +1,38 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    class Program
+    {
+        /*
+         * Write a C# Sharp program to check two given integers, and return true if one of them is 30 or if their sum is 30.
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(30, 15));
+            Console.WriteLine(test(15, 30));
+            Console.WriteLine(test(15, 15));
+            Console.WriteLine(test(14, 18));
+            Console.ReadKey();
+        }
+
+        public static bool test(int x, int y)
+        {
+            if(x == 30 || y == 30 || (x + y) == 30)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+
+
+
+        }
+    }
+}
