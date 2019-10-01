061d0087c316f4d3260d93f53c1cac167550f958
adrien.mechin@gmail.com <adrien.mechin@gmail.com>
2019-10-01 22:18:28 +0200

    Ex10 - Multiple of 3 or 7

---
+++ Ex10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10
+{
+    class Program
+    {
+        /*
+         * Write a C# Sharp program to check if a given positive number is a multiple of 3 or a multiple of 7
+         */
+        static void Main(string[] args)
+        {
+            int nb = 8;
+            Console.WriteLine(test(nb));
+            Console.ReadKey();
+        }
+
+        public static bool test(int nb)
+        {
+            return nb % 3 == 0 || nb % 7 == 0 ? true : false;
+        }
+    }
+}
