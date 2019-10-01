060f61ecdbc512ba90fe1cd89e2feb3e834db1bf
adrien.mechin@gmail.com <adrien.mechin@gmail.com>
2019-10-01 21:37:12 +0200

    Ex7 - Switch first and last letter

---
+++ Ex7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7
+{
+    class Program
+    {
+        /*
+         * Write a C# Sharp program to exchange the first and last characters in a given string and return the new string
+         */
+        static void Main(string[] args)
+        {
+            string input = "Adrien";
+            Console.WriteLine(test(input));
+            Console.ReadKey();
+        }
+
+        public static string test(string str)
+        {
+            if (str.Length > 1)
+            {
+                char first = str[0];
+                char last = str[str.Length - 1];
+                str = str.Remove(0);
+                str = str.Remove(str.Length - 1);
+                str = last.ToString() + str + first.ToString();
+            }
+            return str;
+        }
+    }
+}
