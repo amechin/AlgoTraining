169cd744bc48d1f7d5a1f7febd79140c25df8888
adrien.mechin@gmail.com <adrien.mechin@gmail.com>
2019-10-01 22:34:50 +0200

    Ex18 - Math.Max

---
+++ Ex18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex18
+{
+    class Program
+    {
+        /*
+         *  Write a C# Sharp program to check the largest number among three given integers.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(1, 3, 2));
+            Console.ReadKey();
+        }
+
+        public static int test(int a, int b, int c)
+        {
+            return Math.Max(a, Math.Max(b,c));
+        }
+    }
+}
