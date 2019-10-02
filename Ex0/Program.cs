6b5c45f0dc0af8792226d84b075c5928f165ff62
adrien.mechin@gmail.com <adrien.mechin@gmail.com>
2019-10-02 08:17:34 +0200

    Ex0 - DateTime Format

---
+++ Ex0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString("d"));
+            Console.WriteLine(now.ToString("D"));
+            Console.WriteLine(now.ToString("f"));
+            Console.WriteLine(now.ToString("F"));
+            Console.WriteLine(now.ToString("g"));
+            Console.WriteLine(now.ToString("G"));
+            Console.WriteLine(now.ToString("m"));
+            Console.WriteLine(now.ToString("M"));
+            Console.WriteLine(now.ToString("o"));
+            Console.WriteLine(now.ToString("O"));
+            Console.WriteLine(now.ToString("s"));
+            Console.WriteLine(now.ToString("t"));
+            Console.WriteLine(now.ToString("T"));
+            Console.WriteLine(now.ToString("u"));
+            Console.WriteLine(now.ToString("U"));
+            Console.WriteLine(now.ToString("y"));
+            Console.WriteLine(now.ToString("Y"));
+            Console.ReadLine();
+        }
+    }
+}
