bc3e1962b6ed7252ce9fcf2cd16e41d05bc10b34
adrien.mechin@gmail.com <adrien.mechin@gmail.com>
2019-10-02 13:38:27 +0200

    Ex34 - Sequence of numbers 1, 2, 3 appears in a given array

---
+++ Ex34/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex34
+{
+    class Program
+    {
+        /*
+         * Write a C# Sharp program to check whether the sequence of numbers 1, 2, 3 appears in a given array of integers somewhere
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(new[] { 2, 6, 7, 1, 2, 3, 0, 9 }));
+            Console.ReadKey();
+        }
+
+        public static bool test(int[] tab)
+        {
+            var ans = false;
+            for(int i=0; i<tab.Length; i++)
+            {
+                if(tab[i]==1 && tab[i+1] == 2 && tab[i+2] == 3)
+                {
+                    ans = true;
+                }
+            }
+            return ans;
+        }
+    }
+}
